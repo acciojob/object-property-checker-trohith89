27b56a34b70ab1cc86b1850585f811cb1b8a4957
trohith89 <75958140+trohith89@users.noreply.github.com>
2023-07-03 07:39:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-	for(let i=0; i<sampleObject.length; i++){
-		if(Object.keys(sampleObject)[i] == key){
+	const arr = Object.keys(sampleObject);
+	for(let i=0; i<arr.length; i++){
+		if(arr[i]==key){
 			return true;
 		}
 	}
@@ -11,5 +12,5 @@ function hasKey(key) {
 }
 
 // Do not change the code below
-// const key = prompt("Enter Key.");
-// alert(hasKey(key));
+const key = prompt("Enter Key.");
+alert(hasKey(key));
